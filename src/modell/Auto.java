13e9d069cb9faabcd0c4c5eb422cb128274e4693
konnan933 <95681490+konnan933@users.noreply.github.com>
2023-02-13 09:56:19 +0100

    nev javitasok

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -1,5 +1,5 @@
 
-package jarmuhalad;
+package modell;
 
 
 public class Auto {
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package jarmuhalad;
+package modell;
 
 /**
  * 
@@ -23,7 +23,7 @@ public abstract class Jarmu {
     public boolean tankol(){
         return true;
     }
-    public void halad(){
-    
+    public boolean halad(){
+        return true;
     }
 }
