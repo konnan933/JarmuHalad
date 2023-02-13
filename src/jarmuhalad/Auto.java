3c99e53a117da43f5e0ec5da8c944b18941497f7
konnan933 <95681490+konnan933@users.noreply.github.com>
2023-02-13 09:42:34 +0100

    alapszerkezet

---
+++ src/jarmuhalad/Auto.java
@@ -0,0 +1,14 @@
+
+package jarmuhalad;
+
+
+public class Auto {
+    private boolean defekt;
+    
+    public void kereketCsere(){
+        
+    }
+    public boolean halad(){
+        return true;
+    }
+}
---
+++ src/jarmuhalad/Jarmu.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package jarmuhalad;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public abstract class Jarmu {
+    private boolean beinditva = false;
+    private boolean uzemanyag = true;
+    private boolean megerkezett = false;
+    
+    public void beiindit(){
+    
+    }
+    public void leallit(){
+    
+    }
+    public boolean tankol(){
+        return true;
+    }
+    public void halad(){
+    
+    }
+}
